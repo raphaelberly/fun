4f09a11316f54a1d97928c509362bbaeab432cbc
raphaelbrl <raphael.berly@erento.com>
2016-12-01 13:19:41 +0100

    Added argparser to lunch-roulette/main.py to handle nb_teams argument

--- lunch-roulette/main.py
+++ lunch-roulette/main.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.utils import shuffle
 from time import sleep
 import numpy as np
+import argparse
 
 
 # Create a data frame with participants
@@ -17,11 +18,8 @@ def create_indexes(nb_participants):
     return shuffle(indexes)
 
 
-# MAIN OF SCRIPT
-
-if __name__ == '__main__':
-
-    NUMBER_OF_GROUPS = 7
+# Main function of the script
+def main(nb_teams):
 
     # Load data
     print('\nLoading data...')
@@ -36,9 +34,26 @@ if __name__ == '__main__':
 
     # Assign teams
     print('Assigning teams...')
-    data['Team'] = pd.Series(np.mod(indexes, NUMBER_OF_GROUPS))
+    data['Team'] = pd.Series(np.mod(indexes, nb_teams))
     data = data.sort_values('Team').reset_index(drop=True)
     print('\nRESULT TABLE:\n', data)
 
     data.to_csv('teams.csv', index=False)
     print('\nAll done. Enjoy!')
+
+
+
+# MAIN OF SCRIPT
+
+if __name__ == '__main__':
+
+    # Create a parser to get the number of teams
+    argparser = argparse.ArgumentParser(add_help=False)
+    argparser.add_argument('nb_teams', type=str, help='Number of teams to create')
+
+    # Parse the entered argument
+    nb_teams = int(vars(argparser.parse_args())['nb_teams'])
+
+    # Run the main function of the file
+    main(nb_teams)
+
